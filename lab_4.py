440a0f957f3cf1e6c275e4c94a829cc20bb17671
calipayanj23-alt <calipayanj23@gmail.com>
2026-04-29 11:07:00 +0800

    Add files via upload

---
+++ lab_4.py
@@ -0,0 +1,52 @@
+# integer variables
+a  = 10
+b = 3
+
+print ("INTEGER OPERATIONS")
+print ("addition:", a + b)
+print ("subtraction:", a - b)
+print ("multiplication:", a * b)
+print ("division:", a / b)
+print ("modulus:", a % b)
+print ("exponent:", a ** b)
+print()
+
+# float variables
+x = 10.5
+y = 2.5
+
+print ("FLOAT OPERATIONS")
+print ("addition:", x + y)
+print ("subtraction:", x - y)
+print ("multiplication:", x * y)
+print ("division:", x / y)
+print ("modulus:", x % y)
+print ("exponent:", x ** y)
+print()
+
+# complex variables
+c1 = 2 + 3j
+c2 = 1 + 4j
+
+print ("COMPLEX OPERATIONS")
+print ("addition:", c1 + c2)
+print ("subtraction:", c1 - c2)
+print ("multiplication:", c1 * c2)
+print ("division:", c1 / c2)
+# modulus and exponent operations are not defined for complex numbers
+
+
+# 2 create a variables
+a = 10       # integer
+b = 3.14     # float
+c = 2 + 5j   # complex
+
+#checking the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+
+num1 = 25000000
+num2 = 25_000_000
+print(num1)
+print(num2)
---
+++ lab_6.py
@@ -0,0 +1,24 @@
+#Laboratory exercise 6
+#PART 1: CONDITIONS IN AN IF STATEMENT
+
+number = 10
+if number > 5:
+    print ("condition is true")
+else:   print ("condition is false")
+
+#PART 2: CONDITIONS WITH STRINGS, INT, AND BOOLEAN DATA TYPE
+#LIST: ORDERDED, MUTABLE COLLECTION
+
+my_list = ["apple:", 42, True, "banana", 99, False]
+print ("List:", my_list)
+print ("list length:", len(my_list))
+
+#TUPLE: ORDERED, IMMUTABLE COLLECTION
+my_tuple = ("carrot:", 7, True, "date", 13, False)
+print ("Tuple:", my_tuple)
+print ("tuple length:", len(my_tuple))
+
+#SET UNORDERED, MUTABLE COLLECTION OF UNIQUE ELEMENTS
+my_set = {"elephant", 22, True, "cat", 13, False}
+print ("Set:", my_set)
+print ("set length:", len(my_set))
