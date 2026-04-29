3db3629700921c064e318674f39f24f24c98d97e
calipayanj23-alt <calipayanj23@gmail.com>
2026-04-29 09:54:23 +0800

    Add files via upload

---
+++ grp1_calipayan_ex2.py
@@ -0,0 +1,46 @@
+# My first simple script
+
+# Simple print script
+print("Hello, I'm Jamella Mae Calipayan! 1O")
+
+# -----------------------------------
+# Python Variables
+# Create two variables and assign a number and name
+
+age = 18
+student_name = "Jamella Mae"
+
+print("Age:", age)
+print("Student Name:", student_name)
+
+# -----------------------------------
+# Python - Variable Names
+# First names of group members (excluding your name)
+
+Majesty = "Majesty"
+Aldrin = "Aldrin"
+Mark_Angel = "Mark Angel"
+Raisen = "Raisen"
+Russell = "Russell"
+
+print(Majesty)
+print(Aldrin)
+print(Mark_Angel)
+print(Raisen)
+print(Russell)
+
+# -----------------------------------
+# Many Values to Multiple Variables
+x, y, z = 10, 20, 30
+
+print("x:", x)
+print("y:", y)
+print("z:", z)
+
+# -----------------------------------
+# One Value to Multiple Variables
+a = b = c = 100
+
+print("a:", a)
+print("b:", b)
+print("c:", c)
